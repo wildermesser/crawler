600b4b19892005607449d80e67341060f8239fa8
Alexey Lapukhov <lapuhov.alex@gmail.com>
2018-08-02 11:36:40 +0500

    Add production instance and docker-compose

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -23,3 +23,18 @@ module "jenkins" {
   managed_zone_name = "ocrawler"
   tcp_ports         = ["22", "8080"]
 }
+
+module "production" {
+  source        = "../modules/dockerinstance"
+  instance_name = "production"
+
+  public_key_path   = "${var.public_key_path}"
+  private_key_path  = "${var.private_key_path}"
+  zone              = "${var.zone}"
+  disk_image        = "${var.disk_image}"
+  disk_size         = "10"
+  machine_type      = "${var.machine_type}"
+  dns_zone_name     = "ocrawler.tk."
+  managed_zone_name = "ocrawler"
+  tcp_ports         = ["22", "8000"]
+}
